38c5406ddf3e60852f8ad758e5085be372e816a1
supragya27 <supragya.chauhan001@gmail.com>
2020-06-29 23:55:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'natours' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
