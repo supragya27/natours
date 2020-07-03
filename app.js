030b0b5dd2e6bae61629e6e73b685ea6b0dfaa63
supragya27 <supragya.chauhan001@gmail.com>
2020-07-03 12:07:03 +0530

    added get request to get one tour based on the id mentioned in the url

--- app.js
+++ app.js
@@ -24,6 +24,25 @@ app.get(`/api/v1/tours`, (req, res) => {
   });
 });
 
+app.get(`/api/v1/tours/:id`, (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
