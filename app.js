ec9f0b2ab533eb58bd7faaaa74a91b6a7d18839d
supragya27 <supragya.chauhan001@gmail.com>
2020-08-03 22:50:21 +0530

    mongoose update

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 
 //middlewares
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
