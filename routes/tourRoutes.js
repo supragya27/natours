6898d636fb746b399f79e4d04753713d01478b7f
supragya27 <supragya.chauhan001@gmail.com>
2020-08-04 19:17:02 +0530

    added monthly plan route and controller

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,7 @@ const tourController = require('../controllers/tourController');
 const router = express.Router();
 
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/top-5-tours')
