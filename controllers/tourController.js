d90a9828b184c17ff7cdfd7d1db9ba5fa4925e26
supragya27 <supragya.chauhan001@gmail.com>
2020-08-04 15:35:28 +0530

    limiting fields and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,6 +21,25 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //execute query
     const tours = await query;
     res.status(200).json({
