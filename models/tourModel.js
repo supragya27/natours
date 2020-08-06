408658a55749260ad6c51a0c20db13f28d25159d
supragya27 <supragya.chauhan001@gmail.com>
2020-08-06 17:39:14 +0530

    added built-in validations

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour without name is not allowed'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less than 41 characters'],
+      minlength: [5, 'A tour name must have more than 4 characters'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      min: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
