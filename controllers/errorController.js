93d27cee8dc80264e34f791f66e261e27f1126e1
supragya27 <supragya.chauhan001@gmail.com>
2020-09-12 23:12:32 +0800

    updated

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,64 @@
+const AppError = require('./../utils/appError')
+
+const handleCastError = err => {
+    const message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
+
+const handleDuplicateFields = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+};
+const handleValidationError = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        stack: err.stack,
+        message: err.message,
+    })
+}
+
+const sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            stack: err.stack,
+        })
+    }
+    else {
+        console.error('ERROR ', err)
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong'
+        })
+    }
+}
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    })
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res)
+    }
+    else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err }
+
+
+        if (error.name === 'CastError') { error = handleCastError(error) }
+        if (error.code === 11000) error = handleDuplicateFields(error)
+        if (error.name === 'ValidationError')
+            error = handleValidationError(error);
+        sendErrorProd(error, res)
+    }
 }
--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN..')
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1)
+  })
+})
+
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN..')
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1)
+  })
+})
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -20,6 +36,8 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+
