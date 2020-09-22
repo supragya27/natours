7816495ae427f2507fbaccba78d1cbeab4763d59
supragya27 <supragya.chauhan001@gmail.com>
2020-09-23 02:14:52 +0800

    updated

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authControllers')
+
 const router = express.Router();
 
 router.route('/tour-stats').get(tourController.getTourStats);
@@ -11,7 +13,7 @@ router
 
 router
   .route(`/`)
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 router
   .route(`/:id`)
