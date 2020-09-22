45f4508ac91b46f30385ab856fbb97a47ce638c7
supragya27 <supragya.chauhan001@gmail.com>
2020-09-23 02:14:27 +0800

    password change field update

--- models/userModel.js
+++ models/userModel.js
@@ -30,7 +30,8 @@ const userSchema = new mongoose.Schema({
             },
             message: 'Passwords are not same!'
         }
-    }
+    },
+    passwordChangedAt: Date
 })
 
 userSchema.pre('save', async function (next) {
@@ -44,6 +45,20 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword)
 }
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000,
+            10
+        );
+
+        return JWTTimestamp < changedTimestamp;
+    }
+
+    return false;
+};
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
