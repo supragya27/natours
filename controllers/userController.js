997a095dbc060502abe9ab92041f94dfcc150586
supragya27 <supragya.chauhan001@gmail.com>
2020-09-23 22:18:30 +0530

    added functionality for the user to update his data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('../utils/appError')
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el]
+  })
+  return newObj
+}
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -11,6 +20,26 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //create error if user tries to update password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates!', 400))
+  }
+
+  //update user document
+  const filteredBody = filterObj(req.body, 'name', 'email')
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  })
+
+})
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
