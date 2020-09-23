43e27bf854ff6b2ed4d11a7931284447d40c8053
supragya27 <supragya.chauhan001@gmail.com>
2020-09-23 23:19:44 +0530

    set security http methods

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 
 const errorController = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -9,22 +10,30 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-//middlewares
+// global middlewares
 
+//set security http headers
+app.use(helmet())
+
+//dev logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//limit requests to api
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests! Try again after an hour!'
 })
-
 app.use('/api', limiter)
 
-app.use(express.json());
+//body parser:reading data from body to req.body
+app.use(express.json({ limit: '10kb' }));
+
+//serving static files
 app.use(express.static(`${__dirname}/public`));
+
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
