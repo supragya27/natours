ae1e33f91eae935149850d72d75d50c49e7527e5
supragya27 <supragya.chauhan001@gmail.com>
2020-09-23 23:49:15 +0530

    implemented packages to prevent parameter pollution and carry out data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const errorController = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -30,6 +33,18 @@ app.use('/api', limiter)
 
 //body parser:reading data from body to req.body
 app.use(express.json({ limit: '10kb' }));
+
+//data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+//data sanitization against xss
+app.use(xss())
+
+//prevent parameter pollution
+app.use(hpp({
+  whitelist: ['duration', 'maxGroupSize', 'ratingsAverage', 'ratingsQuantity', 'difficulty', 'price']
+}
+))
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
