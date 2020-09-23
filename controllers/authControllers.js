5d53738320f04f116441dc50803f71149f7a245a
supragya27 <supragya.chauhan001@gmail.com>
2020-09-23 23:02:37 +0530

    sending JWT via cookie

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -14,6 +14,17 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXP_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions)
+
+    user.password = undefined
 
     res.status(statusCode).json({
         status: 'success',
