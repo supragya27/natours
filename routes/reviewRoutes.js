ad8d37ba907dd518a792d35e9dddb7842b61a422
supragya27 <supragya.chauhan001@gmail.com>
2020-09-26 09:31:39 +0530

    added the missing authentication and authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,9 +4,13 @@ const authController = require('./../controllers/authControllers')
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(reviewController.getAllReviews)
-    .post(authController.protect, authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview)
+router.use(authController.protect)
 
-router.route('/:id').get(reviewController.getReview).patch(reviewController.updateReview).delete(reviewController.deleteReview)
+router.route('/').get(reviewController.getAllReviews)
+    .post(authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview)
+
+router.route('/:id').get(reviewController.getReview)
+    .patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
+    .delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,11 +9,15 @@ router.post('/login', authController.login)
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser)
+router.use(authController.protect)
 
-router.patch('/updatePassword', authController.protect, authController.updatePassword)
-router.patch('/updateMe', authController.protect, userController.updateMe)
-router.delete('/deleteMe', authController.protect, userController.deleteMe)
+router.get('/me', userController.getMe, userController.getUser)
+
+router.patch('/updatePassword', authController.updatePassword)
+router.patch('/updateMe', userController.updateMe)
+router.delete('/deleteMe', userController.deleteMe)
+
+router.use(authController.restrictTo('admin'))
 
 router
   .route(`/`)
