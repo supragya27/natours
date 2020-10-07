bb86822927766eb3d11586ede9900af4bf9b92d0
supragya27 <supragya.chauhan001@gmail.com>
2020-10-07 22:51:21 +0530

    Revert "added custom error messages for the required fields"
    This reverts commit 12c9b399578a74da9b9fa872297ff3c09c2ddb1e.

--- models/tourModel.js
+++ models/tourModel.js
@@ -113,9 +113,6 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
-tourSchema.index({ price: 1, ratingsAverage: -1 })
-tourSchema.index({ slug: 1 })
-
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
