7138cc805ba9806ab211c55e21e6d7f554ec1ed5
supragya27 <supragya.chauhan001@gmail.com>
2020-10-09 21:00:08 +0530

    home page update

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview=(req,res)=>{
+const Tour = require('../models/tourModel')
+const catchAsync = require('../utils/catchAsync')
+
+exports.getOverview=catchAsync(async (req,res,next)=>{
+    //get tour data from collection
+    const tours=await Tour.find()
+
+    // build template
+
+    //render template
     res.status(200).render('overview',{
-      title:'All Tours'
+      title:'All Tours',
+      tours
     })
-  }
+  })
 
 exports.getTour=(req,res)=>{
     res.status(200).render('tour',{
