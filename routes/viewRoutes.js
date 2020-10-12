efdba3c0544b65d2e62c2180a02fa18f8218cf85
supragya27 <supragya.chauhan001@gmail.com>
2020-10-12 21:04:57 +0530

    login screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,6 +7,8 @@ const router =express.Router()
   router.get('/',viewController.getOverview )
   
   router.get('/tour/:slug',viewController.getTour)
+
+  router.get('/login',viewController.getLoginForm)
   
 
 module.exports=router
