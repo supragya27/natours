c4e5f30b80f5d7a0b1a899be8daaf24aa5541835
supragya27 <supragya.chauhan001@gmail.com>
2020-10-16 00:10:16 +0530

    updated (bugs present)

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+import '@babel/polyfill'
+import {displayMap} from './mapbox'
+import {login} from './login'
+
+const locations= JSON.parse(document.getElementById('map').dataset.locations);
+
+displayMap(locations)
+
+document.querySelector('.form').addEventListener('submit',e=>{
+    e.preventDefault()
+    const email=document.getElementById('email').value
+    const password=document.getElementById('password').value
+    login(email,password)
+ })
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,39 +1,39 @@
-const locations= JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+export const displayMap ={locations} =>{
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic3VwcmFneWEyNyIsImEiOiJja2czY3dzZG0wOWY3MzNwNDRqdTF0d290In0.m6V2Wd932A0LBHl0_DMUyQ';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3VwcmFneWEyNyIsImEiOiJja2czY3dzZG0wOWY3MzNwNDRqdTF0d290In0.m6V2Wd932A0LBHl0_DMUyQ';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/supragya27/ckg63qmqu48g019k8pbwksn0m',
+       });
+    
+    const bounds=new mapboxgl.LngLatBounds();
+    
+    locations.forEach(loc=>{
+        //creating marker
+        const el=document.createElement('div')
+        el.className='marker';
+    
+        //adding marker
+         new mapboxgl.Marker({
+             element:el,
+             anchor:'bottom'
+         })
+         .setLngLat(loc.coordinates)
+         .addTo(map)
+    
+         
+    
+         //extending the map bounds to include current location
+         bounds.extend(loc.coordinates)
+    })
+    
+    map.fitBounds(bounds,{
+             padding:{
+                 top:200,
+                 bottom:200,
+                 left:100,
+                 right:100
+             }
+         })
+}
 
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/supragya27/ckg63qmqu48g019k8pbwksn0m',
-   });
-
-const bounds=new mapboxgl.LngLatBounds();
-
-locations.forEach(loc=>{
-    //creating marker
-    const el=document.createElement('div')
-    el.className='marker';
-
-    //adding marker
-     new mapboxgl.Marker({
-         element:el,
-         anchor:'bottom'
-     })
-     .setLngLat(loc.coordinates)
-     .addTo(map)
-
-     
-
-     //extending the map bounds to include current location
-     bounds.extend(loc.coordinates)
-})
-
-map.fitBounds(bounds,{
-         padding:{
-             top:200,
-             bottom:200,
-             left:100,
-             right:100
-         }
-     })
