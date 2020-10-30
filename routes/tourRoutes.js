025208aed0550027b270d1e917d9847fb7e4dd01
supragya27 <supragya.chauhan001@gmail.com>
2020-10-30 10:57:21 +0530

    integrated functionality to add tour images

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -34,6 +34,8 @@ router
   .get(tourController.getTour)
   .patch(authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour)
   .delete(authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
